576ec89d7e08a118f04e8e59da6b7792dde2a29f
GabrielBatavia <gabrielbatavia7@gmail.com>
2025-05-02 16:52:22 +0700

    feat(auth): implement login and fetch authenticated user API using JWT
    - Created LoginController for handling user login
    - Implemented authentication using JWT credentials
    - Returned access token upon successful login
    - Added route to fetch current authenticated user (/api/user)
    - Protected /api/user route with auth:api middleware
    - Tested login and user retrieval with Postman

--- POS_PWL/routes/api.php
+++ POS_PWL/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
